7ae0bac5c539f69bf98246632f6d6ace24186692
Giordano Geroldi <Giordano Geroldi>
2016-09-25 02:51:01 +0200

    Initial commit

---
+++ src/main/java/com/tdd/lessons/one/Intro.java
@@ -0,0 +1,7 @@
+package com.tdd.lessons.one;
+
+/**
+ * Created by giord on 25/09/2016.
+ */
+public class Intro {
+}
---
+++ src/test/java/com/tdd/lessons/one/IntroTest.java
@@ -0,0 +1,18 @@
+package com.tdd.lessons.one;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by giord on 25/09/2016.
+ */
+public class IntroTest {
+
+    @Test
+    public void first() throws Exception {
+        Assert.fail();
+
+    }
+}
