e4555016c66b2f4fe1512d5a9c196c0bbda66963
Giordano Geroldi <giordano.geroldi@rumbo.com>
2016-10-01 00:57:05 +0200

    creating tests

--- src/main/java/com/tdd/lessons/one/math/Fraction.java
+++ src/main/java/com/tdd/lessons/one/math/Fraction.java
@@ -10,12 +10,9 @@ public class Fraction
   }
 
   public Fraction plus(Fraction that)
-  { if (that.integerValue != 0 )
-      return that;
-    return this;
+  {
+      return new Fraction(this.integerValue + that.integerValue);
   }
-
-
 
   public int intValue()
   {
