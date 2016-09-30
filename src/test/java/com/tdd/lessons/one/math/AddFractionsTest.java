3024f3e5cbbf0e90665dd9302fcf98e9682ddb4e
Giordano Geroldi <giordano.geroldi@rumbo.com>
2016-10-01 00:59:49 +0200

    creating tests

--- src/test/java/com/tdd/lessons/one/math/AddFractionsTest.java
+++ src/test/java/com/tdd/lessons/one/math/AddFractionsTest.java
@@ -10,25 +10,34 @@ public class AddFractionsTest
   @Test
   public void zeroPlusZero() throws Exception
   {
-        Fraction sum = new Fraction(0).plus(new Fraction(0));
-        assertEquals(0,sum.intValue());
-    }
+    Fraction sum = new Fraction(0).plus(new Fraction(0));
+    assertEquals(0, sum.intValue());
+  }
+
+  @Test
+  public void nonZeroPlusZero() throws Exception
+  {
+    Fraction sum = new Fraction(3).plus(new Fraction(0));
+    assertEquals(3, sum.intValue());
+  }
+
+  @Test
+  public void zeroPlusNonZero() throws Exception
+  {
+    Fraction sum = new Fraction(0).plus(new Fraction(5));
+    assertEquals(5, sum.intValue());
+  }
+
+  @Test
+  public void nonNegativeNonZeroOperands() throws Exception
+  {
+    Fraction sum = new Fraction(3).plus(new Fraction(4));
+    assertEquals(7, sum.intValue());
+  }
 
     @Test
-    public void nonZeroPlusZero() throws Exception {
-        Fraction sum = new Fraction(3).plus(new Fraction(0));
-        assertEquals(3,sum.intValue());
-    }
-
-    @Test
-    public void zeroPlusNonZero() throws Exception {
-        Fraction sum = new Fraction(0).plus(new Fraction(5));
-        assertEquals(5,sum.intValue());
-    }
-
-    @Test
-    public void nonNegativeNonZeroOperands() throws Exception {
-        Fraction sum = new Fraction(3).plus(new Fraction(4));
-        assertEquals(7,sum.intValue());
+    public void NegativeInAndNEgativeOut() throws Exception {
+        Fraction sum = new Fraction(-3).plus(new Fraction(1));
+        assertEquals(-2, sum.intValue());
     }
 }
